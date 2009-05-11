fcc378dc7044b6f840d9e49af0a5c387337aa602
Dr Chris P. Jobling <C.P.Jobling@Swansea.ac.uk>
2009-05-11 09:52:42 +0100

    Made make_dummy_users load data from a fixture

--- db/migrate/004_make_dummy_users.rb
+++ db/migrate/004_make_dummy_users.rb
@@ -1,61 +1,12 @@
+require 'active_record/fixtures'
+
 class MakeDummyUsers < ActiveRecord::Migration
   def self.up
   	down
   	
-    # Admin user ... you're always going to want this one!
-    User.create( # 1
-        :login => 'admin',
-        :password => 'admin',
-        :password_confirmation => 'admin',
-        :title => '',
-        :email => 'EEE.Webmaster@Swansea.ac.uk',
-        :first_name => 'Proman',
-        :last_name => 'Administrator',
-        :staff_or_student_number => '099999')
-        
-    # A dummy student
-    User.create( # 2
-        :login => '000000',
-        :password => 'student',
-        :password_confirmation => 'student',
-        :email => '000000@Swansea.ac.uk',
-        :title => 'Mr',
-        :first_name => 'John',
-        :last_name => 'Doe',
-        :staff_or_student_number => '000000')
-        
-    # A dummy staff member
-    User.create( # 3
-        :login => 'A.N.Academic',
-        :password => 'staff',
-        :password_confirmation => 'staff',
-        :email => 'A.N.Academic@Swansea.ac.uk',
-        :title => 'Prof.',
-        :first_name => 'Anne',
-        :last_name => 'Academic',
-        :staff_or_student_number => '099998')
-    
-    # A dummy coordinator
-    User.create( # 4
-        :login => 'A.Coordinator',
-        :password => 'coordinator',
-        :password_confirmation => 'coordinator',
-        :email => 'A.Coordinator@Swansea.ac.uk',
-        :title => 'Dr.',
-        :first_name => 'Anthony',
-        :last_name => 'Coordinator',
-        :staff_or_student_number => '099997')
-    
-    # A real staff member (Dr Jobling is Module Coordinator)!
-    User.create( # 5
-        :login => 'C.P.Jobling',
-        :password => '07/05/1959',
-        :password_confirmation => '07/05/1959',
-        :email => 'C.P.Jobling@swansea.ac.uk',
-        :title=> 'Dr',
-        :first_name => 'Chris',
-        :last_name => 'Jobling',
-        :staff_or_student_number => '039934')
+    # Create uses from fixture ... see dev_data/README.txt
+    directory = File.join(File.dirname(__FILE__), 'dev_data')
+    Fixtures.create_fixtures(directory, "users")
   end
 
   def self.down
---
+++ db/migrate/dev_data/README.txt
@@ -0,0 +1,8 @@
+Put fixtures that you want to load as migrations into this folder as YML files (e.g. users) then using:
+
+   directory = File.join(File.dirname(__FILE_), 'dev_data')
+   Fixtures.create_fixtures(directory, "your data")
+
+You can load data, defined in the same way as test fixtures, into your development project as part of migrations. Not recommended for production!
+
+Tip from Agile Web Development with Ruby on Rails, 3rd Ed., Page 302. 
