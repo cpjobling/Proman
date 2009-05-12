f0df53b559dc70eb758c3e1dd6fe679282eecbf0
Dr Chris P. Jobling <C.P.Jobling@Swansea.ac.uk>
2009-05-12 08:47:45 +0100

    Added two tests to user_details_test.rb

--- test/unit/user_details_test.rb
+++ test/unit/user_details_test.rb
@@ -9,4 +9,40 @@ class UserDetailsTest < Test::Unit::TestCase
   def test_truth
     assert true
   end
+  
+  def test_invalid_with_empty_atributes
+  	user = User.new
+  	assert !user.valid?
+  	assert user.errors.invalid?(:login)
+  	assert user.errors.invalid?(:email)
+  	assert user.errors.invalid?(:first_name)
+  	assert user.errors.invalid?(:last_name)
+    assert user.errors.invalid?(:staff_or_student_number)
+  end
+  
+  def test_unique_login
+  	user = User.new(:login => users(:academic).login,
+  		:email => "academic@xyz.com",
+  		:first_name => "Jools",
+  		:last_name => "Holland",
+  		:staff_or_student_name => "777777",
+  		:password => "test",
+  		:password_confirmation => "test")
+  		
+  	assert !user.save
+  	assert_equal "has already been taken", user.errors.on(:login)
+  end
+  
+  def test_unique_email
+  	user = User.new(:login => "newuser",
+  		:email => users(:student).email,
+  		:first_name => "Herman",
+  		:last_name => "Munster",
+  		:staff_or_student_name => "777777",
+  		:password => "test",
+  		:password_confirmation => "test")
+  		
+  	assert !user.save
+  	assert_equal "has already been taken", user.errors.on(:email)
+  end
 end
