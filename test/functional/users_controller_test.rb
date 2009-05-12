a0decde6a4b12d6c60e3f45ed59203aa96b0e789
Dr Chris P. Jobling <C.P.Jobling@Swansea.ac.uk>
2009-05-13 00:07:14 +0100

    Added functional tests for validations on user registration

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -64,7 +64,22 @@ class UsersControllerTest < Test::Unit::TestCase
     end
   end  
 
+  def test_should_require_last_name_on_signup
+    assert_no_difference 'User.count' do
+      create_user(:last_name => nil)
+      assert assigns(:user).errors.on(:last_name)
+      assert_response :success
+    end
+  end  
   
+    def test_should_require_staff_or_student_number_on_signup
+    assert_no_difference 'User.count' do
+      create_user(:staff_or_student_number => nil)
+      assert assigns(:user).errors.on(:staff_or_student_number)
+      assert_response :success
+    end
+  end  
+    
 
   protected
     def create_user(options = {})
