f4f4be8a8fa96b8f7d17dd3806ee3511daf2b990
Dr Chris P. Jobling <C.P.Jobling@Swansea.ac.uk>
2009-05-12 20:10:59 +0100

    Added roles tests

--- test/unit/project_test.rb
+++ test/unit/project_test.rb
@@ -8,4 +8,13 @@ class ProjectTest < ActiveSupport::TestCase
   def test_truth
     assert true
   end
+  
+  # Test validation rules
+  def test_invalid_with_empty_atributes
+  	project = Project.new
+  	assert !project.valid?
+  	assert project.errors.invalid?(:title)
+  	assert project.errors.invalid?(:description)
+  end
+  
 end
