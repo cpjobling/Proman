340d19c7301b2aeee3e6c2b8d8c2e136f8a1d742
Dr Chris P. Jobling <C.P.Jobling@Swansea.ac.uk>
2009-05-13 20:36:20 +0100

    Done as much as I want to do for now on project/discipline relationships

--- test/unit/discipline_test.rb
+++ test/unit/discipline_test.rb
@@ -3,15 +3,21 @@ require File.dirname(__FILE__) + '/../test_helper'
 class DisciplineTest < ActiveSupport::TestCase
   fixtures :projects, :disciplines
   
+  def setup
+  	@project = projects(:project1)
+  	@icct = disciplines(:icct)
+  end
+  
   # Replace this with your real tests.
   def test_truth
     assert true
   end
   
-  def test_project_discipline_is_a_discipline
+  def test_projects_disciplines_projects_includes_project
     # test that adding a discipline adds the discipline
-    @project.suitable_for(@icct)
-    assert @project in @icct.projects.find_all
+    @project.suitable_for(@icct.name)
+    the_project = @icct.projects.find_by_title(@project.title)
+    assert @project == the_project
   end
   
   
